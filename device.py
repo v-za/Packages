b4ff2246d7cb5a77a6f2f4579864af837d8a6af0
v-za <za.vinit@gmail.com>
2021-01-07 11:57:42 -0500

    Adding device package for pytorch

---
+++ device.py
@@ -0,0 +1,24 @@
+import torch
+
+
+class Device:
+
+    def __init__(self, device):
+        self.device = device
+        print("initialized!")
+
+
+
+    def set_device(self):
+
+        if self.device:
+            return torch.device('cuda')
+        else:
+            return torch.device('cpu')
+
+    def to_device(self, data,device):
+        'we move our tensor to given device'
+        if isinstance(data, (list,tuple)):
+            return [to_device(x,device) for x in data]
+
+        return data.to(device,non_blocking = True)
