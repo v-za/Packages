454d93bb1063afa75c27ee2e85abbddb7df85208
v-za <za.vinit@gmail.com>
2021-01-07 13:06:30 -0500

    added Data Loader.

---
+++ dataloader.py
@@ -0,0 +1,14 @@
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
