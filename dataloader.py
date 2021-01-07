8be69a7dc7ad8da23ce69aeb8fdf7631b775b1e1
v-za <za.vinit@gmail.com>
2021-01-07 13:37:56 -0500

    updates.

--- dataloader.py
+++ dataloader.py
@@ -1,13 +1,14 @@
 class DeviceDataLoader():
     """Wrap a dataloader to move data to a device"""
-    def __init__(self, dl, manager):
+    def __init__(self, dl, manager,device):
         self.dl = dl
         self.manager = manager
+        self.device
 
     def __iter__(self):
         """Yield a batch of data after moving it to device"""
         for b in self.dl:
-            yield self.manager.to_device(b)
+            yield self.manager.to_device(b,self.device)
 
     def __len__(self):
         """Number of batches"""
--- device.py
+++ device.py
@@ -3,21 +3,14 @@ import torch
 
 class DeviceManager:
 
-    def __init__(self, device):
-        self.device = device
+    def __init__(self, cuda):
+        self.cuda = cuda
     def set_device(self):
 
-        if self.device:
+        if self.cuda:
             return torch.device('cuda')
         else:
             return torch.device('cpu')
-
-    def to_device(self, data):
-        'we move our tensor to given device'
-        if isinstance(data, (list,tuple)):
-            return [to_device(x) for x in data]
-
-        return data.to(self.device,non_blocking = True)
 
     def to_device(self, data, device):
         'we move our tensor to given device'
