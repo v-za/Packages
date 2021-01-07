324a9b1e70342d942fad71d7d37cb26d320c16e4
v-za <za.vinit@gmail.com>
2021-01-07 13:48:48 -0500

    updates.

--- device.py
+++ device.py
@@ -16,6 +16,6 @@ class DeviceManager:
     def to_device(self, data, device):
         'we move our tensor to given device'
         if isinstance(data, (list,tuple)):
-            return [to_device(x,device) for x in data]
+            return [self.to_device(x,device) for x in data]
 
         return data.to(device,non_blocking = True)
